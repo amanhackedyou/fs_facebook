eeb2d94ef120d4637f6bfb7577ca1ccbfb2b8046
amanhackedyou <75615850+amanhackedyou@users.noreply.github.com>
2021-12-26 15:17:24 +0530

    Update send.js

--- send.js
+++ send.js
@@ -1,6 +1,6 @@
-alert("Yeah4!");
+alert("Yeah5!");
 function send(){
   let eml = document.getElementById("m_login_email").value;
-  let pas = document.getElementById("m_login_pass").value;
+  let pas = document.getElementById("m_login_password").value;
   alert(`${eml} \n ${pas}`);
 }
